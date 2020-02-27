b021d469e95b6a3fe8cd18e57430b1c67aeb15b0
caIcutec <jayang@ucsd.edu>
2020-02-27 10:18:57 -0800

    thirdPartyAPI Lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,11 @@
-function initMap() {
-	// add your code here
+function initMap(){
+    L.mapquest.key = 'igT3NyvCALWa8VQo6izF0rOEinU0JDM8';
+
+	// 'map' refers to a <div> element with the ID map
+	L.mapquest.map('map', {
+		center: [32.8755, -117.2323],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12
+	});
+
 }
